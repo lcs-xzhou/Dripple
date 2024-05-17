8d2b132dc04235abfd6fd79292575f90b8c8a9e8
Xinlin Zhou <XZhou@lcs.on.ca>
2024-05-16 23:33:46 -0400

    Initial Commit

---
+++ Dripple/DrippleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrippleApp.swift
+//  Dripple
+//
+//  Created by Fiona ZHOU on 2024-05-16.
+//
+
+import SwiftUI
+
+@main
+struct DrippleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
