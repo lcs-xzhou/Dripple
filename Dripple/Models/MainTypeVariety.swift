584d5e6d8fec85481fa2a0e934c691316b86c250
Xinlin Zhou <XZhou@lcs.on.ca>
2024-05-18 09:27:33 -0400

    I have build a model for the main page of the variety section in order avoid repetition for my main page view.

---
+++ Dripple/Models/MainTypeVariety.swift
@@ -0,0 +1,8 @@
+//
+//  MainTypeVariety.swift
+//  Dripple
+//
+//  Created by Fiona ZHOU on 2024-05-18.
+//
+
+import Foundation
---
+++ Dripple/Views/VarietyView.swift
@@ -0,0 +1,18 @@
+//
+//  VarietyView.swift
+//  Dripple
+//
+//  Created by Fiona ZHOU on 2024-05-18.
+//
+
+import SwiftUI
+
+struct VarietyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VarietyView()
+}
