f93b6c6368fd63c5fc4eb81f50513ab34e4636d3
Xinlin Zhou <XZhou@lcs.on.ca>
2024-05-17 22:10:52 -0400

    I have coded an appropriate template for each item of the variety list, so my next step would be building the list of variety using this template.

---
+++ Dripple/Views/VarietyTypeTemplate.swift
@@ -0,0 +1,18 @@
+//
+//  VarietyTypeTemplate.swift
+//  Dripple
+//
+//  Created by Fiona ZHOU on 2024-05-17.
+//
+
+import SwiftUI
+
+struct VarietyTypeTemplate: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VarietyTypeTemplate()
+}
