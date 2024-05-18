31c52fc1269b0096fff6eb4483716f637b080f43
Xinlin Zhou <XZhou@lcs.on.ca>
2024-05-17 23:29:08 -0400

    I am coding the main menu for the variety now, trying to connect different views together.

---
+++ Dripple/Views/VarietyDetailedTypeView.swift
@@ -0,0 +1,18 @@
+//
+//  VarietyDetailedTypeView.swift
+//  Dripple
+//
+//  Created by Fiona ZHOU on 2024-05-17.
+//
+
+import SwiftUI
+
+struct VarietyDetailedTypeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VarietyDetailedTypeView()
+}
---
+++ Dripple/Views/VarietyMainMenu.swift
@@ -0,0 +1,18 @@
+//
+//  VarietyMainMenu.swift
+//  Dripple
+//
+//  Created by Fiona ZHOU on 2024-05-17.
+//
+
+import SwiftUI
+
+struct VarietyMainMenu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VarietyMainMenu()
+}
