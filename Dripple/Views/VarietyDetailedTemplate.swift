7d4bfdeb4d2ede4a2fe1429286a9becf515c8ce6
Xinlin Zhou <XZhou@lcs.on.ca>
2024-05-17 21:44:41 -0400

    Using the stored strings and values, I am able to create a template view that holds those values with less codes thus avoid repetition.

---
+++ Dripple/Views/VarietyDetailedTemplate.swift
@@ -0,0 +1,18 @@
+//
+//  VarietyDetailedTemplate.swift
+//  Dripple
+//
+//  Created by Fiona ZHOU on 2024-05-17.
+//
+
+import SwiftUI
+
+struct VarietyDetailedTemplate: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VarietyDetailedTemplate()
+}
