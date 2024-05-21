06e3f24158f390a7fb7221b3ba7665d686d551a3
Xinlin Zhou <XZhou@lcs.on.ca>
2024-05-20 20:12:24 -0400

    Adjusted model for a notesItem to use the swift data framework,  and made the mock datas.

---
+++ Dripple/Helpers/Functions1.swift
@@ -0,0 +1,8 @@
+//
+//  Functions1.swift
+//  Dripple
+//
+//  Created by Fiona ZHOU on 2024-05-20.
+//
+
+import Foundation
---
+++ Dripple/Models/NotesItem.swift
@@ -0,0 +1,8 @@
+//
+//  NotesItem.swift
+//  Dripple
+//
+//  Created by Fiona ZHOU on 2024-05-20.
+//
+
+import Foundation
