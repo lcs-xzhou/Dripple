4bcf735506975598d7dab9e6a5f0fa9961684e18
Xinlin Zhou <XZhou@lcs.on.ca>
2024-05-29 12:01:41 -0400

    Added an authentication flow so that users are signed-in anonymously and assigned a unique user id. This prevents one user from seeing another user's data.

---
+++ Dripple/Views/AppEntryView.swift
@@ -0,0 +1,18 @@
+//
+//  AppEntryView.swift
+//  Dripple
+//
+//  Created by Fiona ZHOU on 2024-05-29.
+//
+
+import SwiftUI
+
+struct AppEntryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppEntryView()
+}
