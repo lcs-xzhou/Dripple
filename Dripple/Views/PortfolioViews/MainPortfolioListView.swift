3cc74af56a35301ade00cfbc69d52dffc902174e
Xinlin Zhou <XZhou@lcs.on.ca>
2024-05-30 19:22:54 -0400

    I can now select and load a photo from the Photos library for the user image.

--- Dripple/Views/PortfolioViews/MainPortfolioListView.swift
+++ Dripple/Views/PortfolioViews/MainPortfolioListView.swift
@@ -13,16 +13,66 @@ struct MainPortfolioListView: View {
     // MARK: Stored properties
     // The selection made in the PhotosPicker
     @State var selectionResult: PhotosPickerItem?
-
+    
+    // The actual image loaded from the selection that was made
+    @State var newItemImage: UserImage?
     
     var body: some View {
-        VStack {
-            Image("NotePicture1")
-                .resizable()
-                .scaledToFit()
-                .frame(height: 150)
-            
-            Text("")
+        NavigationView {
+            VStack {
+                HStack {
+                    
+                    PhotosPicker(selection: $selectionResult, matching: .images) {
+                        
+                        // Has an image been loaded?
+                        if let newItemImage = newItemImage {
+                            
+                            // Yes, show it
+                            newItemImage.image
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 150)
+                            
+                        } else {
+                            
+                            // No, show an icon instead
+                            Image(systemName: "photo.badge.plus")
+                                .symbolRenderingMode(.multicolor)
+                                .font(.system(size: 70))
+                                .foregroundStyle(.brown1)
+                                .scaledToFit()
+                            
+                        }
+                    }
+                    
+                }
+                
+                Text("")
+            }
+        }
+        // This block of code is invoked whenever the selection from the picker changes
+        .onChange(of: selectionResult) {
+            // When the selection result is not nil...
+            if let imageSelection = selectionResult {
+                // ... transfer the data from the selection result into
+                // an actual instance of TodoItemImage
+                loadTransferable(from: imageSelection)
+            }
+        }
+    }
+    
+    // MARK: Functions
+    
+    // Transfer the data from the PhotosPicker selection result into the stored property that
+    // will hold the actual image for the new to-do item
+    private func loadTransferable(from imageSelection: PhotosPickerItem) {
+        Task {
+            do {
+                // Attempt to set the stored property that holds the image data
+                newItemImage = try await imageSelection.loadTransferable(type: UserImage.self)
+            } catch {
+                debugPrint(error)
+            }
         }
     }
 }
