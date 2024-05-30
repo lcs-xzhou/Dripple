3eccc31ef5c6a35435b84a1507b953d6c74e824d
Xinlin Zhou <XZhou@lcs.on.ca>
2024-05-30 13:51:09 -0400

    Finished the static layout for a feedback page.

--- Dripple/Views/CountingViews/CountingFeedbackView.swift
+++ Dripple/Views/CountingViews/CountingFeedbackView.swift
@@ -12,10 +12,29 @@ struct CountingFeedbackView: View {
     let feedback: String
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ZStack {
+                Rectangle()
+                    .foregroundColor(.brown1)
+                    .aspectRatio(2, contentMode: .fit)
+                
+                Text(feedback)
+                    .font(.title)
+                    .fontWeight(.semibold)
+                    .fontDesign(.rounded)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+            }
+            
+            Image("CountingPicture1")
+                .resizable()
+                .scaledToFit()
+                .padding(.horizontal)
+        }
+        .padding()
     }
 }
 
 #Preview {
-    CountingFeedbackView()
+    CountingFeedbackView(feedback: "You would need 90 ml of water to make a cup of coffee with 5 grams of coffee powder~")
 }
